545fe14ababff6096f5632a6cf99b1e182752aaf
kovalenko anton <a.kovalenko@etpgpb.ru>
2024-06-25 10:08:21 +0300

    init_structure

---
+++ script.js
@@ -0,0 +1,9 @@
+let sign = prompt("What's your sign?");
+
+if (sign.toLowerCase() === "scorpio") {
+  alert("Wow! I'm a Scorpio too!");
+}
+
+// there are many ways to use the prompt feature
+sign = window.prompt(); // open the blank prompt window
+
