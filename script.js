ec13ccc82329d0c46776930b4fdf94e7b0db43b4
kovalenko anton <a.kovalenko@etpgpb.ru>
2024-06-25 12:35:32 +0300

    added the main algo for singular Playing round + validation of user's input

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 let humanScore = 0;
 let computerScore = 0;
 
+// Функция возвращающая рандомное значение в [0, 1, 2]
 num = function (max = 3) {
     return Math.floor(Math.random() * max);
 }
 
 
+// Функция получающая машинный вариант через промпт и возвращающая его
 function getComputerChoice(){
     result = num();
     if (result == 2){
@@ -20,9 +22,7 @@ function getComputerChoice(){
         return "бумага"
     }
 }
-
-
-
+// Функция получающая челвоеческий вариант через промпт и возвращающая его
 function getHumanChoice(){
     let humanChoice = prompt("Введи свой выбор: ", null)
     humanChoice = humanChoice.toLowerCase();
@@ -30,9 +30,52 @@ function getHumanChoice(){
 }
 
 
-const humanSelection = getHumanChoice();
+// Функция проверяющая правильность ввода промпта со стороны пользователя
+function validateUserInput(userInput) {
+    try {
+      let userInput = getHumanChoice();
+      // Проверка на валидность данных
+      if (userInput == "камень" || userInput == "бумага" || userInput == "ножницы") {
+        return userInput;
+      } else {
+        throw new Error('Некорректный ввод данных');
+      }
+
+    } catch (error) {
+      console.error(`Ошибка при валидации ввода данных: ${error}`);
+      return null;
+    }
+  }
+
+const humanSelection = validateUserInput();
 const computerSelection = getComputerChoice();
 
-console.log("Твой выбор: " + `${humanSelection}` + "\n" +
-            "Выбор компьютера: " + `${computerSelection}`
-)
+
+
+function playRound(realHumanChoice, realComputerChoice){
+    if (realHumanChoice == realComputerChoice) {
+        return "Ты и компьютер выбрали" + " " + `${realHumanChoice}` + ", Ничья!\n"
+        + "Ваш текущий счёт: \n" 
+        + "Вы: " + `${humanScore}` + " | " + "Компьютер: " + `${computerScore}`;
+    }
+    else if ((realHumanChoice == "камень"  && realComputerChoice == "ножницы") 
+            || ( realHumanChoice == "бумага"  && realComputerChoice == "камень")  
+            || ( realHumanChoice == "ножницы" && realComputerChoice == "бумага")){
+                humanScore += 1;
+                return "Ты победил! Компьютер выбрал " + `${realComputerChoice}` + "\n"
+                + "Ваш текущий счёт: \n" 
+                + "Вы: " + `${humanScore}` + " | " + "Компьютер: " + `${computerScore}`;
+    }
+
+    else {
+        computerScore += 1;
+        return "Ты проиграл! Компьютер выбрал " + `${realComputerChoice}`+ "\n"
+        + "Ваш текущий счёт: \n" 
+        + "Вы: " + `${humanScore}` + " | " + "Компьютер: " + `${computerScore}`;
+    }
+
+}
+
+console.log(playRound(humanSelection, computerSelection));
+
+
